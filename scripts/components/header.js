1aed3a9c77f971938909a6d4d0001330009b23a5
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-03-21 14:42:59 -0300

    First Upload

--- scripts/components/header.js
+++ scripts/components/header.js
@@ -9,3 +9,33 @@ window.addEventListener('scroll', () => {
         infiniteArrow.style.transition = 'opacity 0.5s ease-in-out';
     }
 });
+
+const infiniteDescription = document.querySelector('.infinite-description');
+const phrases = [
+    "Mais tempo, mais vida."
+];
+let phraseIndex = 0;
+let charIndex = 0;
+
+function typePhrase() {
+    if (charIndex < phrases[phraseIndex].length) {
+        infiniteDescription.textContent += phrases[phraseIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(typePhrase, 50);
+    } else {
+        setTimeout(erasePhrase, 5000);
+    }
+}
+
+function erasePhrase() {
+    if (charIndex > 0) {
+        infiniteDescription.textContent = phrases[phraseIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erasePhrase, 20);
+    } else {
+        phraseIndex = (phraseIndex + 1) % phrases.length;
+        setTimeout(typePhrase, 150);
+    }
+}
+
+typePhrase();
